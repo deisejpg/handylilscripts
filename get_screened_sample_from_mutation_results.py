903a1cee55a90224612ffc84e0a3689be06fc32f
Deise JP Goncalves <deisejpg@users.noreply.github.com>
2025-01-22 10:41:09 -0500

    Add files via upload

---
+++ get_screened_sample_from_mutation_results.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+df = pd.read_excel('Mutation test results.xlsx', skiprows = 1)
+
+filtered_df = df[df['Result Inference Type Name'] == "Self"]
+
+df2 = pd.read_excel("BioIVT inventory_sample list.xlsx")
+
+merged_df = pd.merge(df2, filtered_df[['Case ID', 'Com ID']], on='Case ID', how='left')
+
+cleaned_df = merged_df.drop_duplicates(subset='Com ID', keep='first')
+
+cleaned_df.to_excel('BioIVT_inventory_sample_list_withScreenedSamples.xlsx', index=False)
